82f61de2fa7805d110510fed27f83c3fe8ea6a45
Sinan Sevgi <sinansevgi@users.noreply.github.com>
2021-09-23 02:18:41 +0300

    Refactor BookForm
     Makes the BooksForm component stateful
     The BooksForm component stores the title and the book category
     handleChange method for handling input
     handleSubmit method for handling submit

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import { CREATE_BOOK } from '../actions';
 
-const BooksForm = () => {
+const BooksForm = ({ submitBook }) => {
+  const [newBook, setNewBook] = useState({
+    title: '',
+    category: 'Action',
+    id: Math.floor(Math.random() * 1000),
+  });
   const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
   const dropdownOptions = categories.map((category) => (
     <option
@@ -10,15 +18,42 @@ const BooksForm = () => {
       {category}
     </option>
   ));
+
+  const handleChange = ({ target: { name, value } }) => {
+    setNewBook({
+      ...newBook,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    submitBook(newBook);
+    setNewBook({
+      title: '',
+      category: 'Action',
+      id: Math.floor(Math.random() * 1000),
+    });
+    document.getElementById('bookForm').reset();
+  };
+
   return (
-    <form action="">
-      <input type="text" placeholder="Book Name" />
-      <select id="categories" name="categories">
+    <form action="" id="bookForm">
+      <input type="text" name="title" placeholder="Book Name" onChange={handleChange} />
+      <select id="categories" name="category" onChange={handleChange}>
         {dropdownOptions}
       </select>
-      <button type="submit">Add Book</button>
+      <button type="submit" onClick={handleSubmit}>Add Book</button>
     </form>
   );
 };
 
-export default BooksForm;
+BooksForm.propTypes = {
+  submitBook: PropTypes.func.isRequired,
+};
+
+export default connect(null, (dispatch) => ({
+  submitBook: (book) => {
+    dispatch(CREATE_BOOK(book));
+  },
+}))(BooksForm);
--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,7 +1,7 @@
 const BookReducer = (state = [], action) => {
   switch (action.type) {
     case 'CREATE_BOOK':
-      return [...state, action.data];
+      return [...state, action.payload];
     case 'REMOVE_BOOK':
       return state.filter((book) => book.title !== action.data.title);
     default:
