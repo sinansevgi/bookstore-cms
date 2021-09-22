ff62e5d836cab4687a58fe36422d9538a76557ba
Sinan Sevgi <sinansevgi@users.noreply.github.com>
2021-09-22 17:42:18 +0300

    Add prop type check

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { PropTypes } from 'prop-types';
 
-const Book = (props) => (
-  <div />
-);
-
+const Book = (props) => {
+  const { book } = props;
+  return (
+    <div>
+      <h2>
+        BOOK NAME :
+        {book.title}
+      </h2>
+      <p>
+        ISBN ID :
+        {book.id}
+      </p>
+      <p>
+        CATEGORY :
+        {book.category}
+      </p>
+    </div>
+  );
+};
 Book.propTypes = {
-
+  book: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default Book;
