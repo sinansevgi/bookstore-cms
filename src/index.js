aca315a2fd56b3b14760c683776efb80b8edd40a
Sinan Sevgi <sinansevgi@users.noreply.github.com>
2021-09-22 14:58:21 +0300

    Create redux store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import mainReducer from './reducers';
+import App from './components/App';
+
+const initialState = {
+  bookList: [],
+};
+
+const store = createStore(mainReducer, initialState);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
