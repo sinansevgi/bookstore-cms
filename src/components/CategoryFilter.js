3935a024f08fc3fd8661bb4ce1c69f8770ee3672
Sinan Sevgi <sinansevgi@users.noreply.github.com>
2021-09-23 03:21:45 +0300

    Add CategoryFilter component

---
+++ src/components/CategoryFilter.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+
+const CategoryFilter = ({ handleFilterChange }) => {
+  const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+  const dropdownOptions = categories.map((category) => (
+    <option
+      key={categories.indexOf(category)}
+      value={category}
+    >
+      {category}
+    </option>
+  ));
+  const handleChange = (event) => {
+    handleFilterChange(event.target.value);
+  };
+  return (
+    <div>
+      <select onChange={handleChange}>
+        {dropdownOptions}
+      </select>
+    </div>
+  );
+};
+
+CategoryFilter.propTypes = {
+  handleFilterChange: PropTypes.func.isRequired,
+};
+export default CategoryFilter;
